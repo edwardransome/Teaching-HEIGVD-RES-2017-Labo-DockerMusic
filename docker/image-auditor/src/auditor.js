3a9478431a2c85f5e3bb817fb8eda07c270e7eaf
Edward Ransome <edward.ransome@heig-vd.ch>
2017-04-30 18:21:56 +0000

    fini

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -5,30 +5,37 @@ var s = dgram.createSocket('udp4');
 
 var musicians = new Map();
 
-s.bind(2205, function(){
-	s.addMembership("239.255.22.5");
-});
-
 s.on('message', function(msg, source){
+	console.log("received" + msg);
 	var musician = JSON.parse(msg);
 	if(musicians.has(musician.uuid)){
-		musicians.get(musician.uuid).lastUpdate = moment.format();
+		musicians.get(musician.uuid).lastUpdate = moment();
 	}else{
-			mus = new Object;
+			mus = new Object();
 			mus.uuid = musician.uuid;
 			mus.instrument = musician.instrument;
-			mus.activeSince = moment.format();
-			mus.lastUpdate = moment.format();
+			mus.activeSince = moment();
+			mus.lastUpdate = moment();
+			musicians.set(mus.uuid, mus);
 	}
+});
+
+s.on('listening', function(){
+  console.log("listening...");
+});
+
+s.bind(2205, function(){
+	s.addMembership("239.255.22.5");
 });
 
 var server = net.createServer(function(socket) {
 	var arrayToSend = [];
 	musicians.forEach( function(value, key, musicians){
-		if(value.lastUpdate > moment().subtract(5, 'seconds')){
-			var mu = new Object;
+		console.log("INSIDE FOREACH");
+		if(value.lastUpdate.isAfter(moment().subtract(5, 'seconds'))){
+			var mu = new Object();
 			mu.uuid = key;
-			mu.intrument = value.intrument;
+			mu.instrument = value.instrument;
 			mu.activeSince = value.activeSince;
 			arrayToSend.push(mu);
 		}
@@ -36,6 +43,7 @@ var server = net.createServer(function(socket) {
 	socket.write(JSON.stringify(arrayToSend)+"\r\n");
 	socket.end();
 });
+
 server.listen(2205);
 
 
